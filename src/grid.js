a67b5600785ab38406ff58d6aae403dd20cdf7f6
Sebastien Bro <sebastien.bro.c@thalesdigital.io>
2018-05-02 17:54:36 +0200

    feat(grid): Init grid

--- src/grid.js
+++ src/grid.js
@@ -1,4 +1,10 @@
 function Grid () {
+  this.width = 7,
+  this.height = 6,
+  this.tokens = new Array(this.width).fill(new Array(this.height).fill(0)),
+  this.checkIfMoveIsAllowed = (column) => {
+    return (column >= 0 && column < this.width && this.tokens[column][this.height]!==0) ? true : false;
+  }
 }
 
 module.exports = Grid;
--- src/grid.spec.js
+++ src/grid.spec.js
@@ -1,10 +1,41 @@
 var expect = require('chai').expect;
 var Grid = require('./grid');
 
-describe('Grid', function() {
+describe('Grid', () => {
   let grid;
 
-  it('', function() {
+  it('should init a Grid object', () => {
+    grid = new Grid();
+    expect(grid).to.be.an.instanceOf(Object);
+  });
+
+  it('should init a tokens array with 7 columns', () => {
+    grid = new Grid();
+    expect(grid.tokens.length).to.equal(7);
+  });
+
+  describe('checkIfMoveIsAllowed', () => {
+    it('it should return true if the colomn provided is an integer between 0 and 6', () => {
+      grid = new Grid();
+      expect(grid.checkIfMoveIsAllowed(0)).to.be.true;
+    });
+
+    it('it should return false if the colomn provided is not an integer', () => {
+      grid = new Grid();
+      expect(grid.checkIfMoveIsAllowed('A')).to.be.false;
+    });
+
+    it('it should return false if the colomn provided is greater than 6', () => {
+      grid = new Grid();
+      expect(grid.checkIfMoveIsAllowed(7)).to.be.false;
+    });
+
+    it.skip('it should return false if the colomn is full', () => {
+      grid = new Grid();
+      grid.tokens[0] = new Array(6).fill(1);
+      console.log(grid);
+      expect(grid.checkIfMoveIsAllowed(0)).to.be.false;
+    });
   });
 
 });
