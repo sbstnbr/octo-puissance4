1816829347030c8643d1441fbf59e8a8da648ae8
Sebastien Bro <sebastien.bro.c@thalesdigital.io>
2018-05-02 18:00:31 +0200

    refactor(grid): Convert to ES6 class

--- src/grid.js
+++ src/grid.js
@@ -1,8 +1,10 @@
-function Grid () {
-  this.width = 7,
-  this.height = 6,
-  this.tokens = new Array(this.width).fill(new Array(this.height).fill(0)),
-  this.checkIfMoveIsAllowed = (column) => {
+class Grid {
+  constructor(){
+    this.width = 7;
+    this.height = 6;
+    this.tokens = new Array(this.width).fill(new Array(this.height).fill(0));
+  }
+  checkIfMoveIsAllowed(column){
     return (column >= 0 && column < this.width && this.tokens[column][this.height]!==0) ? true : false;
   }
 }
