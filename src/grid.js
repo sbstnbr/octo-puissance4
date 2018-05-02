168bfc8c9a3d6558ec98ef249ff3e9819903f8a0
Sebastien Bro <sebastien.bro.c@thalesdigital.io>
2018-05-02 16:56:26 +0200

    chore(project): Initial commit

---
+++ src/grid.js
@@ -0,0 +1,4 @@
+function Grid () {
+}
+
+module.exports = Grid;
---
+++ src/grid.spec.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+var Grid = require('./grid');
+
+describe('Grid', function() {
+  let grid;
+
+  it('', function() {
+  });
+
+});
