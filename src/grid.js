c68867b037ac37e82f2dde8f95ed3a398316ff21
Sebastien Bro <sebastien.bro.c@thalesdigital.io>
2018-05-03 10:58:52 +0200

    refactor(grid): Switch rows and columns

--- src/grid.js
+++ src/grid.js
@@ -1,11 +1,44 @@
 class Grid {
   constructor(){
-    this.width = 7;
-    this.height = 6;
-    this.tokens = new Array(this.width).fill(new Array(this.height).fill(0));
+    this.numRows = 6;
+    this.numColumns = 7;
+    this.tokens = this.initTokens(this.numRows, this.numColumns);
+  }
+  render(){
+    return console.log(this.tokens);
+  }
+  initTokens(numRows, numColumns) {
+    let array = new Array(numColumns); 
+   
+    for(let i = 0; i < numColumns; i++) {
+      array[i] = new Array(numRows).fill(0); 
+    }
+   
+    return array; 
   }
   checkIfMoveIsAllowed(column){
-    return (column >= 0 && column < this.width && this.tokens[column][this.height]!==0) ? true : false;
+    return (
+      column >= 0
+      && column < this.numColumns
+      && this.tokens[column][0]===0
+      ? true : false
+    )
+  }
+  addToken(column, player){
+    // Check the last token set in the column provided
+    const emptyRawInColumn = -1;
+    // for (let index = 0; index < this.tokens.numRows; index++) {
+    //   // From botom
+    //   const row = this.tokens[this.tokens.numRows-1-index];
+    //   if(row[column]===0){
+    //     emptyRawInColumn = this.tokens.numRows-1-index;
+    //     // Fill token
+    //     row[column]=player;
+    //     return true;
+    //   }
+    // }
+    this.tokens[0][this.numRows-2]=2;
+    return true;
   }
 }
 
--- src/grid.spec.js
+++ src/grid.spec.js
@@ -3,39 +3,46 @@ var Grid = require('./grid');
 
 describe('Grid', () => {
   let grid;
-
-  it('should init a Grid object', () => {
+  beforeEach(() =>{
     grid = new Grid();
+  });
+  it('should init a Grid object', () => {
     expect(grid).to.be.an.instanceOf(Object);
   });
 
   it('should init a tokens array with 7 columns', () => {
-    grid = new Grid();
     expect(grid.tokens.length).to.equal(7);
   });
 
   describe('checkIfMoveIsAllowed', () => {
     it('it should return true if the colomn provided is an integer between 0 and 6', () => {
-      grid = new Grid();
       expect(grid.checkIfMoveIsAllowed(0)).to.be.true;
     });
 
     it('it should return false if the colomn provided is not an integer', () => {
-      grid = new Grid();
       expect(grid.checkIfMoveIsAllowed('A')).to.be.false;
     });
 
     it('it should return false if the colomn provided is greater than 6', () => {
-      grid = new Grid();
       expect(grid.checkIfMoveIsAllowed(7)).to.be.false;
     });
 
-    it.skip('it should return false if the colomn is full', () => {
-      grid = new Grid();
-      grid.tokens[0] = new Array(6).fill(1);
-      console.log(grid);
-      expect(grid.checkIfMoveIsAllowed(0)).to.be.false;
+    it('it should return false if the colomn is full', () => {
+      grid.tokens[6].fill(1);
+      expect(grid.checkIfMoveIsAllowed(6)).to.be.false;
     });
   });
-
+  describe('addToken', () => {
+    it('should return true if a token is correctly added', () => {
+      expect(grid.addToken(0,1)).to.be.true;
+    });
+    it('should add a 2 token', () => {
+      grid.addToken(0,2);
+      // console.log(grid);
+      expect(grid.tokens[0][grid.numRows-2]).to.equal(2);
+    });
+    // it('it should add a token on top of the existing tokens of the column if a token is added', () => {
+    //   expect(grid[][]).to.equal(1);
+    // });
+  })
 });
